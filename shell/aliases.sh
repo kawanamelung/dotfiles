4022d65871cc7616d7a3f7e9557e19115defc981
Kawan Amelung <kawan.amelung@gmail.com>
2024-07-18 17:04:57 +0200

    aliases

---
+++ shell/aliases.sh
@@ -0,0 +1,56 @@
+# shell config
+alias d='cd ~/.dotfiles/'
+alias z='nvim ~/.zshrc'
+alias rc='nvim ~/.bashrc'
+alias s='source ~/.zshrc'
+
+# colorize the ls output
+alias ls='ls --color=auto'
+
+# use a long listing format
+alias ll='ls -la'
+
+# show hidden files
+alias l.='ls -d .* --color=auto'
+
+# grep with color
+alias g='grep --color=always'
+
+# clear
+alias c='clear'
+# nvim
+alias nv='nvim ~/.config/nvim/lua/'
+alias swp="cd $HOME/.local/state/nvim/swap"
+
+# history
+alias h='history'
+alias h100='history -n 100'
+
+# env debugging
+alias echop='echo $PATH | tr ":" "\n"'
+alias echopp='echo $PYTHON_PATH | tr ":" "\n"'
+alias pp=' | tr ":" "\n"'
+
+# tmux
+alias t='tmux'
+alias tls='tmux ls'
+alias tk='tmux kill-window'
+alias tks='tmux kill-session'
+alias tka='tmux kill-server'
+alias ta='tmux attach -t'
+alias td='tmux detach'
+alias ts='tmux switch -t'
+alias tt='tmux list-keys'
+
+# git aliases
+alias gl='git pull'
+alias gp='git push'
+alias ga='git add'
+alias gs='git status'
+alias gd='git diff'
+
+# interactive python
+alias ip='ipython'
+
+# gpu
+alias wg='watch -n0.1 nvidia-smi'
